b0d96f0d342943542e1427bcb3a4bd6d67abb459
Tsiky Ny Aina Narindra Fenosoa Malalaharisoa <tsmalala@c1r6s3.42antananarivo.mg>
2026-05-25 15:28:10 +0300

    ex2

--- ex2/oracle.py
+++ ex2/oracle.py
@@ -1,30 +1,63 @@
 import os
 
+
+def print_config(matrix: str, database: str | None, api: str | None,
+                 log: str | None, zion: str | None) -> None:
+    print("\nORACLE STATUS: Reading the Matrix...")
+    print("\nConfiguration loaded:")
+    print(f"Mode: {matrix}")
+    print(f"Database: {database}")
+    print(f"API Access: {api}")
+    print(f"Log Level: {log}")
+    print(f"Zion Network: {zion}")
+
+
 def get_config() -> bool:
     try:
         from dotenv import find_dotenv, load_dotenv
     except Exception as e:
         print(f"[ERROR] {e}")
+        return False
 
     dotenv_path = find_dotenv()
-    if dotenv_path:
-        load_dotenv(dotenv_path)
-        MATRIX_MODE = os.getenv("MATRIX_MODE")
-        DATABASE_URL = os.getenv("DATABASE_URL")
-        API_KEY = os.getenv("API_KEY")
-        LOG_LEVEL = os.getenv("LOG_LEVEL")
-        ZION_ENDPOINT = os.getenv("ZION_ENDPOINT")
+    if not dotenv_path:
+        print("[WARNING] No .env file found")
+        return False
 
-        print("\nORACLE STATUS: Reading the Matrix...")
-        print("\nConfiguration loaded:")
-        print(f"Mode: {MATRIX_MODE}")
-        print(f"Database: {DATABASE_URL}")
-        print(f"API Access: {API_KEY}")
-        print(f"Log Level: {LOG_LEVEL}")
-        print(f"Zion Network: {ZION_ENDPOINT}")
-        return True
-    print("Missing configuration!")
-    return False
+    load_dotenv(dotenv_path)
+    MATRIX_MODE = os.getenv("MATRIX_MODE")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    API_KEY = os.getenv("API_KEY")
+    LOG_LEVEL = os.getenv("LOG_LEVEL")
+    ZION_ENDPOINT = os.getenv("ZION_ENDPOINT")
+
+    required = {
+        "MATRIX_MODE": MATRIX_MODE,
+        "DATABASE_URL": DATABASE_URL,
+        "API_KEY": API_KEY,
+        "LOG_LEVEL": LOG_LEVEL,
+        "ZION_ENDPOINT": ZION_ENDPOINT
+        }
+    for key, values in required.items():
+        if not values:
+            print(f"\n[ERROR] missing {key}")
+            return False
+
+    if not all(required.values()):
+        print("[ERROR] Missing required configuration")
+        return False
+
+    if MATRIX_MODE == "development":
+        print_config(MATRIX_MODE, "Connected to local instance", API_KEY,
+                     "DEBUG", ZION_ENDPOINT)
+    elif MATRIX_MODE == "production":
+        print_config(MATRIX_MODE, "Connected to production server", API_KEY,
+                     "WARNING", ZION_ENDPOINT)
+    else:
+        print("[ERROR] Unknown MATRIX_MODE")
+        return False
+
+    return True
 
 
 if __name__ == "__main__":
